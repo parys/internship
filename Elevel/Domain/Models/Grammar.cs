1bb61385616b03f62700761db53984b7452b1850
rogovandrey <rogovandrey8@gmail.com>
2021-07-07 14:58:41 +0300

    Implement Test and Grammar entities #4

---
+++ Elevel/Domain/Models/Grammar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevel.Domain.Models
+{
+    public class Grammar: BaseDataModel
+    {
+        public Guid QuestionId { get; set; }
+        public Question<Grammar> Question { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+
+        public string Level { get; set; }
+
+        public ICollection<Test> Tests { get; set; }
+
+        public Grammar()
+        {
+            Tests = new List<Test>();
+        }
+    }
+}
---
+++ Elevel/Domain/Models/Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevel.Domain.Models
+{
+    public class Test: BaseDataModel
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid? HrId { get; set; }
+        public User Hr { get; set; }
+
+        public Guid? CoachId { get; set; }
+        public User Coach { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+        public DateTimeOffset AssignmentStartDate { get; set; }
+        public DateTimeOffset AssignmentEndDate { get; set; }
+
+        public ICollection<Grammar> Grammars{ get; set; }
+        public int? GrammarMark { get; set; }
+
+        public Guid? AuditionId { get; set; }
+        public Audition Audition { get; set; }
+        public int? AuditionMark { get; set; }
+
+        public ICollection<UserAnswer> UserAnswers { get; set; }
+
+        public Guid? EssayId{ get; set; }
+        public Essay Essay { get; set; }
+        public string EssayAnswer { get; set; }
+        public int? EssayMark { get; set; }
+
+        public Guid? SpeakingId { get; set; }
+        public Speaking Speaking { get; set; }
+        public string SpeakingAnswerReference { get; set; }
+        public int? SpeakingMark { get; set; }
+
+        public string Comment { get; set; }
+
+        public Test()
+        {
+            UserAnswers = new List<UserAnswer>();
+            Grammars = new List<Grammar>();
+        }
+    }
+}
