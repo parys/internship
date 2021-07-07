cb73f9069ace4b40939b0fd2706c8dec622371c3
Evgenij-Kovpak <evgenij-kowpak@mail.ru>
2021-07-07 20:50:29 +0300

    Create models for UserAnswer, Question and Answer

---
+++ Elevel/Domain/Models/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevel.Domain.Models
+{
+    public class Answer : BaseDataModel
+    {
+        public Guid AnswerId { get; set; }
+        public Guid QuestionId { get; set; }
+        public string NameAnswer { get; set; }
+        public bool IsRight { get; set; }
+    }
+}
