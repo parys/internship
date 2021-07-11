ad9af67d09a5ffbab5e28551bcceb74417aef58e
andreyrogov8 <rogovandrey8@gmail.com>
2021-07-11 17:24:36 +0300

    Andrey Rohau / Uncommented fields in Test, Added relation to ApplicationUser, Configure data base mapping for model Test

--- Elevel/Domain/Models/ApplicationUser.cs
+++ Elevel/Domain/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace Elevel.Domain.Models
 {
@@ -8,5 +9,6 @@ namespace Elevel.Domain.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTimeOffset CreationDate { get; set; }
+        public ICollection<Test> Tests { get; set; } 
     }
 }
