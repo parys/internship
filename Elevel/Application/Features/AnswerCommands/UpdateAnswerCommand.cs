d8c7137e3c0cf3ba936d0189d97760cffea289ba
Zhenya Kurmysa <zhenya.kurmysa@mail.ru>
2021-07-15 19:42:18 +0300

    The commands for Answer entity are created now.

---
+++ Elevel/Application/Features/AnswerCommands/UpdateAnswerCommand.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Elevel.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elevel.Application.Features.TopicCommands
+{
+    public class UpdateAnswerCommand
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid? Id { get; set; }
+            public string NameAnswer { get; set; }
+            public bool IsRight { get; set; }
+            public Guid QuestionId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancelationtoken)
+            {
+                var answer = await _context.Answers.FirstOrDefaultAsync(a => a.NameAnswer== request.NameAnswer && a.IsRight == request.IsRight && a.QuestionId == request.QuestionId, cancelationtoken);
+                if (answer is null)
+                {
+                    throw new NullReferenceException();
+                }
+                answer = _mapper.Map(request, answer);
+
+                await _context.SaveChangesAsync(cancelationtoken);
+                return new Response { Id = answer.Id };
+            }
+        }
+        public class Response
+        {
+            public Guid? Id { get; set; }
+        }
+    }
+}
