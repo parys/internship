0723d4a11678f94f465600dde9dc2d036a479178
Zhenya Kurmysa <zhenya.kurmysa@mail.ru>
2021-07-15 18:52:58 +0300

    The very basement of commands for Topic entity are created now. In the future, I suppose, they would be explored and added.

---
+++ Elevel/Application/Features/TopicCommands/UpdateTopicCommand.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Elevel.Application.Interfaces;
+using Elevel.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elevel.Application.Features.TopicCommands
+{
+    public class Request : IRequest<Response>
+    {
+        public Guid? Id { get; set; }
+        public string TopicName { get; set; }
+        public Level Level { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Request, Response>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public Handler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<Response> Handle(Request request, CancellationToken cancelationtoken)
+        {
+            var topic = await _context.Topics.FirstOrDefaultAsync(a => a.TopicName == request.TopicName && a.Level == request.Level && a.CreationDate == request.CreationDate, cancelationtoken);
+            if (topic is null)
+            {
+                throw new NullReferenceException();
+            }
+            topic = _mapper.Map(request, topic);
+
+            await _context.SaveChangesAsync(cancelationtoken);
+            return new Response { Id = topic.Id };
+        }
+    }
+    public class Response
+    {
+        public Guid? Id { get; set; }
+    }
+}
