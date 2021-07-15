ebb5f11da8815aaa8af3c368ef4c56d4a8334506
Iryna Kazlova <rinchan603@gmail.com>
2021-07-15 21:12:21 +0300

    Implement pagination

---
+++ Elevel/Application/Pagination/PagedBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Elevel.Application.Pagination
+{
+    public abstract class PagedBase
+    {
+        /// <summary>
+        /// Item count on page
+        /// </summary>
+        public int PageSize { get; set; } = 10;
+
+        /// <summary>
+        /// Current page number
+        /// </summary>
+        public int CurrentPage { get; set; } = 1;
+
+        public int SkipCount() => (CurrentPage - 1) * PageSize;
+    }
+}
