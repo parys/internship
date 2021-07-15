a4dc4842102b4c0000edfbf6bba33778aad468ca
rogovandrey <rogovandrey8@gmail.com>
2021-07-15 15:19:30 +0300

    Andrey Rohau / Deploy

--- Elevel/Elevel.Api/Startup.cs
+++ Elevel/Elevel.Api/Startup.cs
@@ -93,16 +93,18 @@ namespace Elevel.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elevel.Api v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elevel.Api v1"));
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            context.Database.Migrate();
 
             app.UseRouting();
             app.UseAuthentication();
