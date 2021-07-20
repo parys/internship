cb7f121dbf09f4a837c5fd40173dce76780153de
andreyrogov8 <53875869+andreyrogov8@users.noreply.github.com>
2021-07-20 15:17:04 +0300

    Andrey Rohau / Implemented Get All Application Users Query (#75)
    * Andrey Rohau / Implemented GetAllApplicationUsersQuery
    
    * Delete UpdateTopicCommand.cs
    
    * Authorize only for HR
    
    * restored CreateTopicCommand
    
    * Restored Update Topic
    
    * Added Search by LastName, FirstName, CreationDate, User Id
    
    * Erased search by CreationDate
    
    * get data from DB
    
    * Last Changes
    Ifs became more redable,
    copy-paste is not used anymore,
    Exceprion is returned when user == null,
    Enum is used in controller,
    Self added mediatr has been removed from controller
    
    * Cleaning
    
    * Last Changes
    
    removed unused variables,
    simplified conditions,
    dublications avoided,
    Users async methods,
    Serialization removed,
    Keeping query components in right order,
    Using mapping
    Mapping changed
    
    * Added migration for Priority prop,
    Simplified authorization
    
    * Added UsersDTO
    
    Co-authored-by: parys <andrew_parys@mail.ru>

---
+++ Elevel/Elevel.Api/Controllers/ApplicationUserController.cs
@@ -0,0 +1,28 @@
+﻿using Elevel.Application.Features.ApplicationUserFeatures;
+using Elevel.Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Elevel.Api.Controllers
+{
+    [Authorize(Roles = nameof(UserRole.HumanResourceManager))]
+    [Route("api/[controller]")]
+    public class ApplicationUserController : BaseApiController
+    {
+        [HttpGet]
+        public  async Task<IActionResult> GetAllUsersAsync([FromQuery] GetAllApplicationUserQuery.Request request )
+        {
+            var result = Mediator.Send(request);
+            return Ok(await result);
+        }
+
+        [HttpGet("{Id:Guid}")]
+        public async Task<IActionResult> GetUsersByIdAsync([FromRoute] GetApplicationUserByIdQuery.Request request)
+        {
+            var result = Mediator.Send(request);
+            return Ok(await result);
+        }
+    }
+}
