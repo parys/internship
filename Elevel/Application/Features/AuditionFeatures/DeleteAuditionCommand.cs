12449fa5d75175bd42b7c10fa74cdf6a2c281716
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-22 18:02:34 +0500

    Audition commands, queries and endpoints

---
+++ Elevel/Application/Features/AuditionFeatures/DeleteAuditionCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Elevel.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elevel.Application.Features.AuditionFeatures
+{
+   public class DeleteAuditionCommand
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            public Handler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancelationtoken)
+            {
+                var audiotion = await _context.Auditions.FirstOrDefaultAsync(a => a.Id == request.Id, cancelationtoken);
+                if (audiotion is null)
+                {
+                    return null;
+                }
+                _context.Auditions.Remove(audiotion);
+
+                await _context.SaveChangesAsync(cancelationtoken);
+                return new Response { Id = audiotion.Id };
+            }
+        }
+        public class Response
+        {
+            public Guid? Id { get; set; }
+        }
+    }
+}
---
+++ Elevel/Application/Features/AuditionFeatures/GetAuditionListQuery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Elevel.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Elevel.Application.Pagination;
+using AutoMapper.QueryableExtensions;
+
+namespace Elevel.Application.Features.AuditionFeatures
+{
+    public class GetAuditionListQuery
+    {
+        public class Request : PagedQueryBase, IRequest<Response>
+        {
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var audition = _context.Auditions.AsNoTracking();
+
+                return new Response
+                {
+                    PageSize = request.PageSize,
+                    CurrentPage = request.CurrentPage,
+                    Results = await audition.Skip(request.SkipCount())
+                    .Take(request.PageSize)
+                    .ProjectTo<AuditionDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken),
+                    RowCount = await audition.CountAsync(cancellationToken)
+                };
+            }
+        }
+        [Serializable]
+        public class Response : PagedResult<AuditionDto>
+        {
+
+        }
+        [Serializable]
+        public class AuditionDto
+        {
+            public Guid Id { get; set; }
+            public string AudioFilePath { get; set; }
+            public DateTimeOffset CreationDate { get; set; }
+        }
+    }
+}
