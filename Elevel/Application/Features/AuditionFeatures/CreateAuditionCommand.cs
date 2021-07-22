cf660602c37c32db2a2e92c9eba35bf713caa52b
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-22 18:02:34 +0500

    Audition commands, queries and endpoints

---
+++ Elevel/Application/Features/AuditionFeatures/CreateAuditionCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Elevel.Application.Interfaces;
+using Elevel.Domain.Models;
+using MediatR;
+
+namespace Elevel.Application.Features.AuditionFeatures
+{
+    public class CreateAuditionCommand
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancelationtoken)
+            {
+                var audiotion = _mapper.Map<Audition>(request);
+
+                _context.Auditions.Add(audiotion);
+                await _context.SaveChangesAsync(cancelationtoken);
+
+                return new Response { Id = audiotion.Id, CreationDate = DateTime.UtcNow };
+            }
+        }
+        public class Response
+        {
+            public Guid? Id { get; set; }
+            public DateTimeOffset CreationDate { get; set; }
+        }
+    }
+}
---
+++ Elevel/Elevel.Api/Controllers/AuditionController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Elevel.Application.Features.AuditionFeatures;
+using MediatR;
+
+namespace Elevel.Api.Controllers
+{
+
+    [Authorize, Route("[controller]/[action]")]
+    public class AuditionController : BaseApiController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuditionAsync([FromBody] CreateAuditionCommand.Request request)
+        {
+            var res = await Mediator.Send(request);
+            return Ok(res);
+        }
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> UpdateAuditionAsync([FromRoute] Guid id, [FromBody] UpdateAuditionCommand.Request request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+            var res = await Mediator.Send(request);
+            return Ok(res);
+        }
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> DeleteAuditionAsync([FromRoute] Guid id, [FromBody] DeleteAuditionCommand.Request request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest();
+            }
+            var res = await Mediator.Send(request);
+            return Ok(res);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAuditionList([FromBody] GetAuditionListQuery.Request request)
+        {
+            return Ok(await Mediator.Send(request));
+        }
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetAuditionById([FromBody] GetAuditionByIdQuery.Request request)
+        {
+            return Ok(await Mediator.Send(request));
+        }
+    }
+}
+
