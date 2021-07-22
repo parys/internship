e88ee02c4c241893d38befb102200d2bac05844d
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-22 18:02:34 +0500

    Audition commands, queries and endpoints

---
+++ Elevel/Application/Features/AuditionFeatures/GetAuditionByIdQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Elevel.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elevel.Application.Features.AuditionFeatures
+{
+    public class GetAuditionByIdQuery
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var audition = await _context.Auditions.AsNoTracking()
+                    .ProjectTo<Response>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(m => m.Id == request.Id, cancellationToken);
+                if (audition is null)
+                {
+                    return null;
+                }
+                return audition;
+            }
+        }
+        [Serializable]
+        public class Response
+        {
+            public Guid Id { get; set; }
+            public string AudioFilePath { get; set; }
+            public DateTimeOffset CreationDate { get; set; }
+        }
+    }
+}
---
+++ Elevel/Application/Features/AuditionFeatures/UpdateAuditionCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Elevel.Application.Interfaces;
+using AutoMapper;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elevel.Application.Features.AuditionFeatures
+{
+    public class UpdateAuditionCommand
+    {
+        public class Request : IRequest<Response>
+        {
+            public Guid Id { get; set; }
+            public string AudioFilePath { get; set; }
+        }
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly IApplicationDbContext _context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Response> Handle(Request request, CancellationToken cancelationtoken)
+            {
+                var audiotion = await _context.Auditions.FirstOrDefaultAsync(a => a.AudioFilePath == request.AudioFilePath && a.Id == request.Id, cancelationtoken);
+                if (audiotion is null)
+                {
+                    return null;
+                }
+
+                audiotion = _mapper.Map(request, audiotion);
+
+                await _context.SaveChangesAsync(cancelationtoken);
+                return new Response { Id = audiotion.Id };
+            }
+        }
+        public class Response
+        {
+            public Guid? Id { get; set; }
+        }
+    }
+}
