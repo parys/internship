00b44748b4faacc5b2fb4980ac97e2fc38038de2
rogovandrey <rogovandrey8@gmail.com>
2021-08-18 19:27:57 +0300

    Andrey Rohau / Refactor email notification

---
+++ Elevel/Application/Infrastructure/Configurations/EmailFormConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevel.Application.Infrastructure.Configurations
+{
+    public class EmailFormConfiguration
+    {
+        public List<MailboxAddress> ReceiverEmail { get; set; } = new List<MailboxAddress>();
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
--- Elevel/Elevel.Infrastructure.Services/Jobs/EmailJob.cs
+++ Elevel/Elevel.Infrastructure.Services/Jobs/EmailJob.cs
@@ -1,4 +1,5 @@
-﻿using Elevel.Application.Infrastructure.Configurations;
+﻿using Elevel.Application.Extensions;
+using Elevel.Application.Infrastructure.Configurations;
 using Elevel.Application.Interfaces;
 using Elevel.Domain.Models;
 using Elevel.Infrastructure.Services.Implementation;
@@ -6,8 +7,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using Quartz;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,21 +19,81 @@ namespace Elevel.Infrastructure.Services.Jobs
     public class EmailJob : IJob
     {
         private IMailService _mail;
+        private readonly IApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
 
         public EmailJob(IServiceScopeFactory serviceScopeFactory, IOptions<EmailConfigurations> emailConfiguration)
         {
             var service = (serviceScopeFactory.CreateScope()).ServiceProvider;
 
-            var userManager = service.GetService<UserManager<User>>();
+            _userManager = service.GetService<UserManager<User>>();
 
-            var context = service.GetService<IApplicationDbContext>();
+            _context = service.GetService<IApplicationDbContext>();
 
-            _mail = new MailService(userManager, emailConfiguration, context);
+            _mail = new MailService(_userManager, emailConfiguration);
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
-            await _mail.SendNotificationsToHrsAndUsersAsync();
+            var userEmails = await _context.Tests
+                .Where(x => x.AssignmentEndDate.HasValue
+                    && DateTimeOffset.Compare(x.AssignmentEndDate.Value.Date, DateTimeOffset.UtcNow.Date) >= 0)
+                .AsNoTracking()
+                .Select(x => MailboxAddress.Parse(
+                    _userManager.Users.FirstOrDefault(u => u.Id == x.UserId).Email))
+                .AsNoTracking()
+                .ToListAsync();
+
+            var missedDeadlineUsers = _context.Tests
+                .Where(x => x.AssignmentEndDate.HasValue
+                    && DateTimeOffset.Compare(x.AssignmentEndDate.Value.Date, DateTimeOffset.UtcNow.Date) < 0
+                    && !x.GrammarMark.HasValue)
+                .Select(x => new
+                {
+                    HrEmail = MailboxAddress.Parse(
+                    _userManager.Users.FirstOrDefault(u => u.Id == x.HrId).Email),
+                    UserName = _userManager.Users.FirstOrDefault(u => u.Id == x.UserId).GetUserNames()
+                });
+
+            var missedDict = new Dictionary<MailboxAddress, string>();
+
+            foreach (var hr in missedDeadlineUsers)
+            {
+                if (!missedDict.ContainsKey(hr.HrEmail))
+                {
+                    missedDict.Add(hr.HrEmail, hr.UserName);
+                }
+                else
+                {
+                    missedDict[hr.HrEmail] = missedDict[hr.HrEmail] + $",\n{hr.UserName}";
+                }
+            }
+
+            var emailForms = new List<EmailFormConfiguration>();
+
+            var userEmailForm = new EmailFormConfiguration();
+            if (userEmails.Count > 0)
+            {
+                userEmailForm.ReceiverEmail.AddRange(userEmails);
+                userEmailForm.Subject = "Elevel test reminder";
+                userEmailForm.Body = "You have assigned for test. \nCheck your profile.";
+                emailForms.Add(userEmailForm);
+            }
+            if (missedDict.Count > 0) {
+                foreach (var hr in missedDict)
+                {
+                    var hrEmailForm = new EmailFormConfiguration();
+
+                    hrEmailForm.ReceiverEmail.Add(hr.Key);
+                    hrEmailForm.Subject = "Missed deadline users";
+                    hrEmailForm.Body = $"There are some users have missed the deadline:\n{hr.Value}";
+
+                    emailForms.Add(hrEmailForm);
+                }
+            }
+            if (emailForms.Count > 0) {
+                _mail.UsersEmailNotification(emailForms);
+            } 
         }
     }
 }
