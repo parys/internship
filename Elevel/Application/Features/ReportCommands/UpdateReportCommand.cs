d1a097646ac1a99f1087f712e2e832629e9fa98e
Iryna Kazlova <rinchan603@gmail.com>
2021-08-22 14:22:14 +0300

    Delete CoachId from updateReport

--- Elevel/Application/Features/ReportCommands/UpdateReportCommand.cs
+++ Elevel/Application/Features/ReportCommands/UpdateReportCommand.cs
@@ -20,8 +20,6 @@ namespace Elevel.Application.Features.ReportCommands
         public class Request : IRequest<Response>
         {
             public Guid Id { get; set; }
-            [JsonIgnore] 
-            public Guid CoachId { get; set; }
             public ReportStatus ReportStatus { get; set; }
         }
 
@@ -49,11 +47,6 @@ namespace Elevel.Application.Features.ReportCommands
             {
                 var report = await _context.Reports
                     .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
-
-                if (report.CreatorId != request.CoachId)
-                {
-                    throw new ValidationException("You can't change this report status");
-                }
 
                 if (report is null)
                 {
