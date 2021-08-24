468027e3757ae7f29c25cd2de56c9283faa028d8
rogovandrey <rogovandrey8@gmail.com>
2021-08-24 20:04:25 +0300

    Report validaion

--- Elevel/Application/Features/ReportCommands/UpdateReportCommand.cs
+++ Elevel/Application/Features/ReportCommands/UpdateReportCommand.cs
@@ -60,6 +60,11 @@ namespace Elevel.Application.Features.ReportCommands
                     throw new ValidationException("You can't change this report status");
                 }
 
+                if(report.ReportStatus == ReportStatus.Fixed || report.ReportStatus == ReportStatus.Declined)
+                {
+                    throw new ValidationException("This report has already been solved");
+                }
+
                 if (report is null)
                 {
                     throw new NotFoundException(nameof(Report), request.Id);
