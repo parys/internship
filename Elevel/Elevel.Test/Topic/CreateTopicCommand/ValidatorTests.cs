4427d43f5550c02562bb57bc4fa5d504aaa9a373
Evgenij-Kovpak <58892723+Evgenij-Kovpak@users.noreply.github.com>
2021-08-30 01:00:36 +0300

    Create some Tests (#176)
    Co-authored-by: евгений ковпак <evgenij-kowpak@outlook.com>
    Co-authored-by: parys <andrew_parys@mail.ru>

---
+++ Elevel/Elevel.Test/Topic/CreateTopicCommand/ValidatorTests.cs
@@ -0,0 +1,79 @@
+﻿using Validator = Elevel.Application.Features.TopicCommands.CreateTopicCommand.Validator;
+using Request = Elevel.Application.Features.TopicCommands.CreateTopicCommand.Request;
+using Xunit;
+using FluentValidation.TestHelper;
+using Elevel.Domain.Enums;
+
+namespace Elevel.Test.Topic.CreateTopicCommand
+{
+    
+    public class ValidatorTests
+    {
+        private readonly Validator _validator;
+
+        public ValidatorTests()
+        {
+            _validator = new Validator();
+        }
+
+        [Theory]
+        [InlineData((string)null)]
+        [InlineData("")]
+        public void RuleForTopicName_WhenTopicNameIsNullOrEmpty_ShouldHaveValidationError(string value)
+        {
+            var model = new Request
+            {
+                TopicName = value
+            };
+            var result = _validator.TestValidate(model, opt => opt.IncludeProperties(x => x.TopicName));
+            result.ShouldHaveValidationErrorFor(x => x.TopicName);
+        }
+
+        [Fact]
+        public void RuleForTopicName_WhenValueLengthIs30_ShouldNotHaveValidationError()
+        {
+            var value = new string('1', 30);
+            var model = new Request
+            {
+                TopicName = value
+            };
+            var result = _validator.TestValidate(model, opt => opt.IncludeProperties(x => x.TopicName));
+            result.ShouldNotHaveValidationErrorFor(x => x.TopicName);
+        }
+
+       [Fact]
+        public void RulleForLevel_WhenLevelIsEmpty_ShouldHaveValidationError()
+        {
+            var model = new Request
+            {
+                Level = new Level()
+            };
+            var result = _validator.TestValidate(model, opt => opt.IncludeProperties(x => x.Level));
+            result.ShouldHaveValidationErrorFor(x => x.Level);
+        }
+
+        [Theory]
+        [InlineData((Level)5)]
+        public void RuleForLevel_WhenCountIs5_ShouldNotHaveValidationError(Level value)
+        {
+            var model = new Request
+            {
+                Level = value
+            };
+            var result = _validator.TestValidate(model, opt => opt.IncludeProperties(x => x.Level));
+            result.ShouldNotHaveValidationErrorFor(nameof(model.Level));
+        }
+
+        [Theory]
+        [InlineData((Level)5)]
+        public void RuleForLevel_WhenCountIs6_ShouldHaveValidationError(Level value)
+        {
+            var model = new Request
+            {
+                Level = value
+            };
+            var result = _validator.TestValidate(model, opt => opt.IncludeProperties(x => x.Level));
+            result.ShouldNotHaveValidationErrorFor(nameof(model.Level));
+        }
+    }
+}
---
+++ Elevel/Elevel.Test/Topic/GetTopicList/HandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using Elevel.Application.Interfaces;
+using Moq;
+using Xunit;
+using Handler = Elevel.Application.Features.TopicCommands.GetTopicListQuery.Handler;
+using Request = Elevel.Application.Features.TopicCommands.GetTopicListQuery.Request;
+using Response = Elevel.Application.Features.TopicCommands.GetTopicListQuery.Response;
+
+
+namespace Elevel.Test.Topic.GetTopicList
+{
+    public class HandlerTests
+    {
+        
+    }
+}
